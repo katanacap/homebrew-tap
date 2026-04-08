68184373228f873cdc4f09358da8317bfcc69179
Evgeniy Abramov <i@kio.ninja>
2026-04-08 05:46:58 +0400

    bump kitak to v3.2.1

--- Formula/kitak.rb
+++ Formula/kitak.rb
@@ -1,8 +1,8 @@
 class Kitak < Formula
   desc "Vanity address generator for Ethereum, Bitcoin, and Solana"
   homepage "https://github.com/katanacap/kitak"
-  url "https://github.com/katanacap/kitak/archive/refs/tags/v3.2.0.tar.gz"
-  sha256 "78a154131c8285b9c79f61603de69b226cc1aad702800d835aeab5f578fd52d2"
+  url "https://github.com/katanacap/kitak/archive/refs/tags/v3.2.1.tar.gz"
+  sha256 "6df806804150564926b23db7b140e428953749ffa25fcd0b0407836e9ea1b649"
   license "Apache-2.0"
 
   depends_on "rust" => :build
